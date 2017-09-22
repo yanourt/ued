bb97c29b921668d802a7ab0fd053f8d8005eac4a
Yann <potdeyaourt@live.fr>
2017-09-22 15:06:41 +0200

    Menu before big changes

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,26 @@
 window.addEventListener('load', function(){
 
     var projectLink = document.getElementById('projects'),
-        navigation = document.getElementById('navigation');
+        navigation = document.getElementById('navicon'),
+        backBtn = document.getElementById('backbtn'),
+        menuBtn = document.getElementById('menubtn');
 
     projectLink.addEventListener('click', function(e){
         e.preventDefault();
         navigation.classList.toggle('proj-open');
-    })
+    });
+
+    menuBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        navigation.classList.toggle('nav-open');
+    });
+
+    backBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        navigation.classList.toggle('proj-open');
+    });
 
 
     // var logo = document.getElementById('logo'),
